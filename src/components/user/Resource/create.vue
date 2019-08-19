9f98576971b10478b5316a9216c057d1582b3a07
railken <railken@protonmail.ch>
2019-08-20 01:24:39 +0200

    updates

--- src/components/user/Resource/create.vue
+++ src/components/user/Resource/create.vue
@@ -1,8 +1,8 @@
 <template>
   <q-resource-create v-bind="$attrs">
     <template slot='create' slot-scope="scope">
-      <q-text v-model="scope.resource" :attribute="scope.config.getAttribute('name')" :errors="scope.errors"/>
-      <q-text v-model="scope.resource" :attribute="scope.config.getAttribute('email')" :errors="scope.errors"/>
+      <q-attr-text v-model="scope.resource" :attribute="scope.config.getAttribute('name')" :errors="scope.errors"/>
+      <q-attr-text v-model="scope.resource" :attribute="scope.config.getAttribute('email')" :errors="scope.errors"/>
       <q-secret v-model="scope.resource" :attribute="scope.config.getAttribute('password')" :errors="scope.errors"/>
     </template>
   </q-resource-create>
--- src/components/user/Resource/edit.vue
+++ src/components/user/Resource/edit.vue
@@ -1,8 +1,8 @@
 <template>
   <q-resource-edit v-bind="$attrs">
     <template slot='edit' slot-scope="scope">
-      <q-text v-model="scope.resource" :attribute="scope.config.getAttribute('name')" :errors="scope.errors"/>
-      <q-text v-model="scope.resource" :attribute="scope.config.getAttribute('email')" :errors="scope.errors"/>
+      <q-attr-text v-model="scope.resource" :attribute="scope.config.getAttribute('name')" :errors="scope.errors"/>
+      <q-attr-text v-model="scope.resource" :attribute="scope.config.getAttribute('email')" :errors="scope.errors"/>
       <q-secret v-model="scope.resource" :attribute="scope.config.getAttribute('password')" :errors="scope.errors"/>
     </template>
   </q-resource-edit>
